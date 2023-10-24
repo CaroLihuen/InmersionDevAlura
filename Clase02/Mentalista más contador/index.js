23297aada86f31760e8e25abe7fdfe7079310a3d
CaroLihuen <carolinaldominguez@outlook.com>
2023-10-24 17:26:44 -0300

    add exercises

--- Clase02/Mentalista más contador/index.js
+++ Clase02/Mentalista más contador/index.js
@@ -1,12 +1,33 @@
-let numeroSecreto = parseInt(Math.random()*1000 + 1);
+const jugar = prompt('¿Quieres jugar? (Si/No)')
+const gracias = alert("Gracias! Nos vemos en el próximo juego!")
 
-while (numeroSecreto != numeroDigitado) {
-   var numeroDigitado = prompt("Ingrese un número entre 1 y 1000")
-    if (numeroSecreto == numeroDigitado) {
-        alert("Acertaste")
-    } else if (numeroSecreto > numeroDigitado) {
-        alert(`Te equivocaste .. el número secreto es mayor que el número ${numeroDigitado}`)
-    } else if (numeroSecreto < numeroDigitado) {
-        alert(`Te equivocaste .. el número secreto es menor que el número ${numeroDigitado}`)
+if (jugar == 'Si') {
+    let numeroSecreto = parseInt(Math.random() * 500 + 1);
+    let intentos = 0;
+    let numeroDigitado;
+    let intentosMax = 10;
+
+    while (intentos < intentosMax) {
+        numeroDigitado = prompt(`Ingrese un número entre 1 y 500. Tienes ${intentosMax} intentos!`)
+        
+        intentos++;
+        console.log(intentos)
+        let intentosRestantes = intentosMax - intentos;
+
+        if (numeroSecreto == numeroDigitado) {
+            alert(`Acertaste en ${intentos} intentos`);
+            break;
+        } else if (numeroSecreto > numeroDigitado) {
+            alert(`Te equivocaste .. el número secreto es mayor que el número ${numeroDigitado}. Te quedan ${intentosRestantes} intentos.`)
+        } else if (numeroSecreto < numeroDigitado) {
+            alert(`Te equivocaste .. el número secreto es menor que el número ${numeroDigitado}. Te quedan ${intentosRestantes} intentos.`)
+        }
     }
+
+    if(intentos == intentosMax){
+        alert(`Agotaste tus ${intentosMax} intentos. El número secreto era ${numeroSecreto}`)
+    }
+} else {
+    gracias 
 }
+
