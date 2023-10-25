2938706b7cb66be795836874e24701e9abe5f2d2
CaroLihuen <carolinaldominguez@outlook.com>
2023-10-25 16:27:51 -0300

    add movies

---
+++ Clase03/Copia netflix/index.js
@@ -0,0 +1,14 @@
+let listaPeliculas = [
+    'https://2.bp.blogspot.com/-KdOZ8zOrSsA/UVT4Sm4G1OI/AAAAAAAAAEQ/PZOtknEMUps/s1600/English_Spirited_Away_Poster_by_behruz.jpg',
+    'http://cartelesmix.es/images/CartelesH/hombrebicentenario99001.jpg',
+    'https://series-y-peliculas.com/wp-content/uploads/2018/09/la-monja-2-768x432.jpeg',
+    'https://viureview.com.br/images/filmes8/La-la-land.jpg',
+    'https://i0.wp.com/moviefail.com/wp-content/uploads/2012/05/avengers-4.jpeg'
+    ];
+
+
+for (let i = 0; i < listaPeliculas.length; i++) {
+   document.write(`<img src= ${listaPeliculas[i]} >`)
+    
+}
+
